999ea25bf93d905b448a73835d8cec9459d596dc
shikha Mishra <shikha99569@gmail.com>
2023-09-05 12:14:20 +0530

    attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public  static void main(String[] args){
+        RWOnly obj = new RWOnly();
+        obj.name="Shikha";
+        obj.setName("Shikha");
+        obj.getName();
+    }
   
 }
